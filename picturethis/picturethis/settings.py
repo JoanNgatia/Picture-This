abb72c00bef0a3cae578cf4f8e178a29ff81728b
andela-jngatia <joan.ngatia@andela.com>
2016-04-19 23:07:33 +0300

    [Chore #117944195] Set up POSTGRES DB configurations

--- picturethis/picturethis/settings.py
+++ picturethis/picturethis/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # internal apps
+    'imageeditor',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -76,8 +78,12 @@ WSGI_APPLICATION = 'picturethis.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'picturethis',
+        'USER': 'joan',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
