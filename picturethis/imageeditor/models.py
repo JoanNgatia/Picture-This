11de05ad1dbd62e4ad5c4ef055f2e367d5656f54
andela-jngatia <joan.ngatia@andela.com>
2016-04-22 10:24:15 +0300

    [Feature #117944429] Create base User and Image model

--- picturethis/imageeditor/models.py
+++ picturethis/imageeditor/models.py
@@ -1,5 +1,19 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Photo(models.Model):
+    """Create base model for Photo details."""
+
+    owner = models.ForeignKey(User, related_name='photos')
+    image = models.ImageField(upload_to='myphotos/')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        """Extend parent meta class."""
+
+        ordering = ['-updated_at']
