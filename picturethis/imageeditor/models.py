f2eb4e9c5ec5c52bb45fbef1f67a54cc82dfc364
andela-jngatia <joan.ngatia@andela.com>
2016-04-22 15:47:30 +0300

    [Chore #117944429] Refine models for edited images

--- picturethis/imageeditor/models.py
+++ picturethis/imageeditor/models.py
@@ -5,10 +5,26 @@ from django.contrib.auth.models import User
 
 
 class Photo(models.Model):
-    """Create base model for Photo details."""
+    """Create base model for original Photo details."""
 
     owner = models.ForeignKey(User, related_name='photos')
     image = models.ImageField(upload_to='myphotos/')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        """Extend parent meta class."""
+
+        ordering = ['-updated_at']
+
+
+class EditedPhoto(models.Model):
+    """Base model for photos that have been edited/filtered through."""
+
+    image = models.ImageField(upload_to='editedphotos/')
+    parent_image = models.ForeignKey(Photo)
+    effect = models.CharField(max_length=80)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
