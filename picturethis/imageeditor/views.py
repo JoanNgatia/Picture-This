3c9a047f9f9cd31890efdbba92a3c1ee4b4321f7
andela-jngatia <joan.ngatia@andela.com>
2016-04-26 01:04:31 +0300

    [Chore #117945691]Write boilerplate code for image views

--- picturethis/imageeditor/views.py
+++ picturethis/imageeditor/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from models import Photo, EditedPhoto
 
-# Create your views here.
+
+class DashboardView(TemplateView):
+    """Main display of original and edited photos."""
+
+    template_name = 'dash/dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        """Return dictionary representing passed in context."""
+        context = super(DashboardView, self).get_context_data(**kwargs)
+        context['photos'] = Photo.objects.all()
+        context['edited_photos'] = EditedPhoto.objects.all()
