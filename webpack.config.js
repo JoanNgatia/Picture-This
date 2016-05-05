1b9e17a459e3119a906d3b36203500b73547f0fa
andela-jngatia <joan.ngatia@andela.com>
2016-05-05 11:56:29 +0300

    [Feature #117945253]Add webpack bundling capabilities to serve JS files

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require("path");
+var webpack = require('webpack');
+var BundleTracker = require('webpack-bundle-tracker');
+module.exports = {
+    context: __dirname,
+    entry: [
+      "./picturethis/static/js/app.jsx"
+    ],
+    output: {
+      path: __dirname + '/picturethis/static/js/build',
+      filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.jsx?$/, loaders: ['react-hot', 'babel-loader', 'eslint-loader'], exclude: /node_modules/ }, // to transform JSX into JS
+        ]
+    },
+    plugins: [
+      new BundleTracker({filename: './webpack-stats.json'}),
+    ],
+    resolve: {
+        modulesDirectories: ['node_modules'],
+        extensions: ['', '.js', '.jsx']
+    },
+};
