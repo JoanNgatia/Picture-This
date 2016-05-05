d018829062c8d42bc8623f9c35ffbd918ada4d0c
andela-jngatia <joan.ngatia@andela.com>
2016-05-05 15:03:36 +0300

    [Chore #117945253]Add materialize styling for components

--- picturethis/static/js/components/home.jsx
+++ picturethis/static/js/components/home.jsx
@@ -1,10 +1,25 @@
 import { render } from 'react-dom'
 import React, {Component} from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+injectTapEventPlugin();
 
 class Home extends Component {
     render () {
         return (
-            <h1>Hello World!!</h1>
+            <div>
+                  <nav>
+                    <div className="nav-wrapper">
+                      <a href="#" className="brand-logo">Picture-This</a>
+                      <ul className="right hide-on-med-and-down">
+                        <li><a href="#"><i className="material-icons">search</i></a></li>
+                        <li><a href="#"><i className="material-icons">refresh</i></a></li>
+                        <li><a href="#"><i className="material-icons">more_vert</i></a></li>
+                      </ul>
+                    </div>
+                  </nav>
+                <h6>Hello World!!</h6>
+            </div>
         );
     }
 }
