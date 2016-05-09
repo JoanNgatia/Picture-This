a37b4970ae921bcdf064451f2cd5901668ce0834
andela-jngatia <joan.ngatia@andela.com>
2016-05-09 22:24:58 +0300

    [Feature #117945253] Add entry JSX element

--- picturethis/static/js/components/home.jsx
+++ picturethis/static/js/components/home.jsx
@@ -5,23 +5,34 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 
 class Home extends Component {
-    render () {
-        return (
-            <div>
-                  <nav>
-                    <div className="nav-wrapper">
-                      <a href="#" className="brand-logo">Picture-This</a>
-                      <ul className="right hide-on-med-and-down">
-                        <li><a href="#"><i className="material-icons">search</i></a></li>
-                        <li><a href="#"><i className="material-icons">refresh</i></a></li>
-                        <li><a href="#"><i className="material-icons">more_vert</i></a></li>
-                      </ul>
-                    </div>
-                  </nav>
-                <h6>Hello World!!</h6>
-            </div>
-        );
-    }
+  render () {
+      return (
+          <div>
+                <nav>
+                  <div className="nav-wrapper">
+                    <a href="#" className="brand-logo">Picture-This</a>
+                    <ul className="right hide-on-med-and-down">
+                      <li><a href="#"><i className="material-icons">search</i></a></li>
+                      <li><a href="#"><i className="material-icons">refresh</i></a></li>
+                      <li><a href="#"><i className="material-icons">more_vert</i></a></li>
+                    </ul>
+                  </div>
+                </nav>
+              <div className="row">
+                  <div className="col s12 m4 l3">
+                      <div id="editedphotos">
+                      </div>
+                  </div>
+                  <div className="col s12 m8 l9">
+                      <div id="originalphotos">
+                        <h1>Hello there!!!</h1>
+                        <h4>That's all there is for now</h4>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      );
+  }
 }
 
 module.exports = Home;
