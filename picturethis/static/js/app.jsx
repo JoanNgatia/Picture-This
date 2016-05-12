8996d4e995ae524b886ca1bc30957b098351122b
andela-jngatia <joan.ngatia@andela.com>
2016-05-12 19:23:34 +0300

    [Feature #17945405] Integrate Facebook JS SDK functionality

--- picturethis/static/js/app.jsx
+++ picturethis/static/js/app.jsx
@@ -1,15 +1,27 @@
-import React, {Component} from 'react';
+// import React, {Component} from 'react';
+// import { render } from 'react-dom'
+// import Home from './components/home.jsx';
+
+// class App1 extends React.Component {
+//   render() {
+//     return (
+//         <div>
+//             <Home />
+//         </div>
+//     );
+//   }
+// }
+
+// render(<App1/>, document.getElementById('home'))
 import { render } from 'react-dom'
+import { Router, Route, Link, browserHistory, IndexRoute} from 'react-router';
+import React, {Component} from 'react';
 import Home from './components/home.jsx';
+import Auth from './components/auth.jsx';
 
-class App1 extends React.Component {
-  render() {
-    return (
-        <div>
-            <Home />
-        </div>
-    );
-  }
-}
+let routes = (<Router history={browserHistory}>
+                <Route path="/" component={Auth}/>
+                <Route path="/home" component={Home}/>
+              </Router>)
 
-render(<App1/>, document.getElementById('home'))
+render(routes, document.getElementById('home'));
