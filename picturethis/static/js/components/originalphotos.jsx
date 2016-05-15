1666d6dd6988b55cc6a4ad41eed685b37bcabd66
andela-jngatia <joan.ngatia@andela.com>
2016-05-15 21:56:48 +0300

    [Fix #117945603]Fix rendering of original photos

--- picturethis/static/js/components/originalphotos.jsx
+++ picturethis/static/js/components/originalphotos.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 class OriginalPhotoList extends React.Component {
+    // set original component state
     constructor() {
         super();
         this.state = {
@@ -8,20 +9,12 @@ class OriginalPhotoList extends React.Component {
         this._fetchoriginalPhotos = this._fetchoriginalPhotos.bind(this);
     }
 
+    // set image render on component load on page
     componentDidMount(){
          this._fetchoriginalPhotos();
     }
 
-    render(){
-        const originalphotos = this._getoriginalPhotos();
-        return(
-            <div className="photos-list">
-                {originalphotos}
-            </div>
-        );
-    }
-
-
+    // collect all photos from server
     _fetchoriginalPhotos() {
         $.ajax({
             type: 'GET',
@@ -34,7 +27,7 @@ class OriginalPhotoList extends React.Component {
         });
     }
 
-
+    // map out all photos returned from server to the single photo component
     _getoriginalPhotos(){
         return this.state.originalphotos.map((originalphoto) => {
             return (<OriginalPhoto
@@ -42,14 +35,24 @@ class OriginalPhotoList extends React.Component {
                 body={originalphoto.image}/>);
         });
     }
+
+    render(){
+        const originalphotos = this._getoriginalPhotos();
+        return(
+            <div className="photos-list">
+                {originalphotos}
+            </div>
+        );
+    }
 }
 
+// single photo component
 const OriginalPhoto  = (props) => {
 
         return(
             <div className="original-photos">
                 <h5>Image names</h5>
-                <img src={props.body}/>
+                <img src={props.body} width="400" height="400"/>
             </div>
         );
 
