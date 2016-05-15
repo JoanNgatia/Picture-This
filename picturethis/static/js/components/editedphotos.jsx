02eba0b5f615df162cc522a6b295bb2732e50e0b
andela-jngatia <joan.ngatia@andela.com>
2016-05-15 22:06:30 +0300

    [Fix #117945603]Fix rendering of pre-filtered photos

--- picturethis/static/js/components/editedphotos.jsx
+++ picturethis/static/js/components/editedphotos.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 class EditedPhotoList extends React.Component {
+    // set initial state of component
     constructor() {
         super();
         this.state = {
@@ -8,8 +9,31 @@ class EditedPhotoList extends React.Component {
         this._fetcheditedPhotos = this._fetcheditedPhotos.bind(this);
     }
 
+    // check that component is rendered with all edited photos
     componentDidMount(){
         this._fetcheditedPhotos();
+    }
+
+    // Fetch prerendered filters from server
+    _fetcheditedPhotos() {
+        $.ajax({
+            type: 'GET',
+            url: '/api/edits/',
+            success: (editedphotos) => {
+                console.log(this.state);
+                this.setState({editedphotos: editedphotos });
+                console.log(editedphotos);
+            }
+        });
+    }
+
+    // map prerendered filters to single photo component
+    _geteditedPhotos(){
+        return this.state.editedphotos.map((editedphoto) => {
+            return (<EditedPhoto
+                key={editedphoto.id}
+                body={editedphoto.file_path}/>);
+        });
     }
 
     render(){
@@ -20,42 +44,15 @@ class EditedPhotoList extends React.Component {
             </div>
         );
     }
-
-    _fetcheditedPhotos() {
-        $.ajax({
-            type: 'GET',
-            url: '/api/edits/',
-            success: (editedphotos) => {
-                // this.setState({originalphotos});
-                console.log(this.state);
-                this.setState({editedphotos: editedphotos });
-                console.log(editedphotos);
-            }
-        });
-    }
-
-    _geteditedPhotos(){
-        return this.state.editedphotos.map((editedphoto) => {
-            return (<EditedPhoto
-                key={editedphoto.id}
-                body={editedphoto.file_path}/>);
-        });
-    }
 }
 
-class EditedPhoto extends React.Component {
-    constructor() {
-        super();
-    }
-
-    render(){
-        return(
-            <div className="filteredphotos">
-                <h5>Filtered Image names</h5>
-                <img src={this.props.body}/>
-            </div>
-        );
-    }
+const EditedPhoto = (props) => {
+    return(
+        <div className="filteredphotos">
+            <h5>Filtered Image </h5>
+            <img src={props.body} width="400" height="400"/>
+        </div>
+    );
 }
 
 export default EditedPhotoList
