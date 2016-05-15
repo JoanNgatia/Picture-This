fc979cd837596fd6b60f96398af1e8fcb29c66f9
andela-jngatia <joan.ngatia@andela.com>
2016-05-15 23:36:04 +0300

    [Chore #117945603]Edit image view layout

--- picturethis/static/js/components/home.jsx
+++ picturethis/static/js/components/home.jsx
@@ -13,12 +13,16 @@ const Home = () => {
             <div className="nav-wrapper">
               <a href="#!" className="brand-logo">Picture-This</a>
               <ul className="right hide-on-med-and-down">
-                <li><a href="/logout">Log-Out<i className="fa fa-sign-out" aria-hidden="true"></i></a></li>
+                <li><a href="/logout">Log-Out <i className="fa fa-sign-out" aria-hidden="true"></i></a></li>
               </ul>
             </div>
           </nav>
           <div className="row">
             <div className="col s12 m4 l3">
+              <ul className="tabs">
+                <li className="tab col s6 active "><a href="#">Home</a></li>
+                <li className="tab col s6"><a href="#">Profile</a></li>
+              </ul>
               <OriginalPhotoList />
             </div>
             <div className="col s12 m8 l9">
