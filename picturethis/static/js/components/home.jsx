9ced89375a88beac3a31945c317e9e4fd2673460
andela-jngatia <joan.ngatia@andela.com>
2016-05-25 21:43:01 +0300

    [Chore #120224701]Restyle navbar

--- picturethis/static/js/components/home.jsx
+++ picturethis/static/js/components/home.jsx
@@ -7,7 +7,7 @@ class Home extends React.Component {
   render(){
       return (
         <div>
-          <nav>
+          <nav className="navigation">
             <div className="nav-wrapper">
               <a href="#!" className="brand-logo">Picture-This</a>
               <ul className="right hide-on-med-and-down">
